4cd489389e42c751eb0a83c4dafef996b042cf30
gopal7881 <rajareddy.orcl@gmail.com>
2017-09-20 06:13:33 +0530

    Nagesh Programe Change-Id: I8f50b39a932900397745f11ffd8d3c4118eb225f Signed-off-by: gopal7881 <rajareddy.orcl@gmail.com>

---
+++ gopal/src/raj/Nagesh_Scope.java
@@ -0,0 +1,12 @@
+package raj;
+
+public class Nagesh_Scope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String s="nagesh";
+     String s2="nagesh";
+     System.out.println(s==s2);
+	}
+
+}
